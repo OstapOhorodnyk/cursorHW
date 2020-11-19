91b1eeeff6849a887c31208cdc135c4e6cb512c2
OstapOhorodnyk <63155914+OstapOhorodnyk@users.noreply.github.com>
2020-11-19 18:02:52 +0200

    abb some hint

--- hm2.js
+++ hm2.js
@@ -1,8 +1,9 @@
+//отримуємо перше число
 let numberN = +prompt("Введіть ціле початкове число N:");
 while (!Number.isInteger(numberN)) {
   numberN = +prompt("Введене число повинне бути цілим. Введіть число ще раз:");
 }
-
+//отримуємо друге число
 let numberM = +prompt("Введіть друге ціле число M:");
 while (!Number.isInteger(numberM)) {
   lastNumber = +prompt(
@@ -14,9 +15,9 @@ while (numberM < numberN) {
     "Друге число повиннне бути більшим за перше, введіть число ще раз:"
   );
 }
-
+//визначаємо які числа складувати
 const numbersForAddition = confirm("Складаємо парні чи непарні числа?");
-
+//основне складання
 let resultOfAddition = 0;
 for (let i = numberN; i <= numberM; i++) {
   if (numbersForAddition && i % 2 === 0) continue;
